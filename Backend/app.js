fa012012a0dd612a99555ddaa01cab904fac3fd7
srt-1909 <sachinthakkar1992002@gmail.com>
2025-02-08 23:53:17 +0530

    a

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.use(cors());
+
+
+module.exports = app;
